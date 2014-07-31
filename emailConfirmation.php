fffe88d82e7e28f038f5b0e7df529fba18c881ca
alielabridi <elabridiali@outlook.com>
2014-07-31 21:25:16 +0100

    emailConfirmation correction bug

--- emailConfirmation.php
+++ emailConfirmation.php
@@ -1,50 +1,50 @@
 <?php
 /*
   verified the validity of the adress e-mail
-  GET email to send confirmation email
+  GET id to send confirmation email
   GET code to confirm email entered
   ECHO message to get feedback
 */
 require_once("connect.php");
 
-if(isset($_GET["email"]) AND !isset($_GET["code"])){
+if(isset($_GET["id"]) AND !isset($_GET["code"])){
     $db = new mysqli($hostname_mysqli,$username_mysqli,$password_mysqli,$database_mysqli);
     if($db->connect_error){
       die("Connect error ({$db->connect_errno}) {$db->connect_error}");
     }
-    $email = $db->real_escape_string($_GET['email']);
-    $sql = "SELECT usr_email FROM `userapps` WHERE usr_email = '$email';";
+    $id = $db->real_escape_string($_GET['id']);
+    $sql = "SELECT usr_email,confirm_code FROM `userapps` WHERE Facebook_ID = '$id';";
     $result = $db->query($sql);
     if($result->num_rows==0)
-      $message = "$email does not exist in our database ! Please, <a href=\"Register.php\">Sign in</a>";
+      $message = "This ID does not exist in our database ! Please, <a href=\"Register.php\">Sign in</a>";
     else if($result->num_rows==1)
     {
-      $message = "A confirmation e-mail has been send to $send. Please, check your inbox!";
       $row = $result->fetch_assoc();
+      $message = "A confirmation e-mail has been send to {$row["usr_email"]}. Please, check your inbox!";
 
       /* E-mail prepation */
     $subject = 'Welcome to Pikelife.com';
     $msg = '<h1>Welcome to Pikelife $row["usr_fname"] $row["usr_lname"]</h1>';
     $msg .= '<span>Confirm your e-mail to get started with the pikelife experience</span>';
-    $msg .= "<a href=\"http://pikelife.com/emailConfirmation.php?email=$email&code={$row["confirm_code"]}\"";
+    $msg .= "<a href=\"http://pikelife.com/emailConfirmation.php?id={$_GET['id']}&code={$row["confirm_code"]}\"";
     $headers = 'From: noreply@pikelife.com' . "\r\n" .
     'Reply-To: noreply@pikelife.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-    mail($email, $subject, $msg, $headers);
+    mail($row["usr_email"], $subject, $msg, $headers);
     }
 }
-else if(isset($_GET["code"]) AND isset($_GET["email"])){
+else if(isset($_GET["code"]) AND isset($_GET["id"])){
     $db = new mysqli($hostname_mysqli,$username_mysqli,$password_mysqli,$database_mysqli);
     if($db->connect_error){
       die("Connect error ({$db->connect_errno}) {$db->connect_error}");
     }
-    $email = $db->real_escape_string($_GET['email']);
+    $id = $db->real_escape_string($_GET['id']);
     $code = $db->real_escape_string($_GET['code']);
-    $sql = "SELECT * FROM `usrapps` WHERE usr_email = '$email' AND confirm_code = '$code';";
+    $sql = "SELECT * FROM `userapps` WHERE `Facebook_ID` = '41384774' AND `confirm_code` = '722613783'";
     $result = $db->query($sql);
-    if($result->num_row==1)
+    if($result->num_rows==1)
     {
-      $sql = "UPDATE `userapps` SET confirmed = '1' WHERE usr_email ='$email' AND confirm_code = '$code';";
+      $sql = "UPDATE `userapps` SET confirmed = '1' WHERE Facebook_ID = '$id' AND confirm_code = '$code';";
       $db->query($sql);
       $message = "You have successfully confirmed you e-mail thank you";
     }
@@ -59,7 +59,7 @@ else if(isset($_GET["code"]) AND isset($_GET["email"])){
 <html>
 
 <head>
-	<title>Email Confirmation</title>
+	<title>email Confirmation</title>
 <style>
 body 
 {
